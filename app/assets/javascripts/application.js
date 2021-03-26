6321e5abac1f364d7af9ef5a9ba9e63422791917
Rediane Zemmouri <z.rediane24@gmail.com>
2021-03-26 13:45:05 +0100

    Add navigation and favicon

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,13 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
 //= require activestorage
 //= require turbolinks
+//= require semantic-ui
 //= require_tree .
+
+
+$(document).on("turbolinks:load", ()=>{
+    $('.ui.dropdown').dropdown();
+})
